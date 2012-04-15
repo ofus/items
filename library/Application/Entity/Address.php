4b29abc169dcfd60ae3b9af9a84ec6472403c239
Andrew Joseph <andrew@aljweb.com>
2012-04-15 03:15:47 -0500

    namespace

--- library/Application/Entity/Address.php
+++ library/Application/Entity/Address.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Entities;
+namespace Application\Entity;
 
 /** @Entity @Table(name="addresses") */
 class Address
--- library/Application/Entity/User.php
+++ library/Application/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Entities;
+namespace Application\Entities;
 
 /** @Entity @Table(name="users") */
 class User
